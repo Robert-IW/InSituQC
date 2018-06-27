5a11e7da52e5b85437ae48e09525944db21e2b8c
Robert William Schlegel <robwschlegel@gmail.com>
2018-06-27 19:53:16 +0200

    Graphing work

--- graph/diff.R
+++ graph/diff.R
@@ -2,6 +2,7 @@
 # Load libraries ----------------------------------------------------------
 
 library(tidyverse)
+library(ggpubr)
 library(FNN)
 
 
@@ -21,10 +22,15 @@ site_coords <- site_list %>%
 ## OISST
 load("data/OISSt_inbound.RData")
 
+## Bathymetry
+load("data/bathy_mask.Rdata")
+bathy_mask_sub <- bathy_mask %>% 
+  filter(depth <= 0)
+rm(bathy_mask)
 
 # Create means ------------------------------------------------------------
 
-breaks <- seq(10,28,2)
+breaks <- seq(8,28,2)
 
 pal_1 <- c("#49AEB5", "#41B0AC", "#3EB1A3", "#41B198", "#48B28C", "#52B181",
            "#5EB175", "#6BAF69", "#78AE5E", "#85AC54", "#92A94B", "#9FA643",
@@ -47,25 +53,69 @@ SACTN_cut <- SACTN_daily_v4.2 %>%
   dplyr::summarise(mean_temp = mean(temp, na.rm = T)) %>%
   dplyr::mutate(temp_cut = cut(mean_temp, breaks = breaks)) %>% 
   left_join(site_coords, by = "index") %>% 
-  dplyr::ungroup()
+  dplyr::ungroup() %>% 
+  dplyr::select(lon, lat, everything())
 # SACTN_cut <- as.data.frame(SACTN_cut)
 
 # Calculate differences ---------------------------------------------------
 
-match_index <- knnx.index(data =  as.matrix(OISST_cut[,1:2]),
-                          query = as.matrix(SACTN_cut[,5:6]), k = 3)
-OISST_sub <- data.frame(sub_1 = OISST_cut$mean_temp[match_index[,1]],
-                        sub_2 = OISST_cut$mean_temp[match_index[,2]],
-                        sub_3 = OISST_cut$mean_temp[match_index[,3]])
-OISST_sub <- OISST_sub %>% 
-  mutate(sub = sum(sub_1))
+# NB: Lon/lat must be the first and second columns of the dataframe in that order
+df_is <- SACTN_cut
+df_sst <- OISST_cut
+product_compare <- function(df_is, df_sst){
+  
+  # Spread the in situ data by season
+  df_is_wide <- df_is %>% 
+    dplyr::select(-temp_cut) %>% 
+    tidyr::spread(key = season, value = mean_temp)
+  
+  # Spread the sst data by season
+  df_sst_wide <- df_sst %>% 
+    dplyr::select(-temp_cut) %>% 
+    tidyr::spread(key = season, value = mean_temp)
+  
+  # Find the nearest three matching pixels
+  match_index <- knnx.index(data =  as.matrix(df_sst_wide[,1:2]),
+                            query = as.matrix(df_is_wide[,1:2]), k = 3)
+  
+  # Grab the winter temperatures
+  df_sst_winter <- data.frame(sub_1 = df_sst_wide$Winter[match_index[,1]],
+                           sub_2 = df_sst_wide$Winter[match_index[,2]],
+                           sub_3 = df_sst_wide$Winter[match_index[,3]]) %>% 
+    dplyr::mutate(sub = rowMeans(.))
+  
+  # Grab the summer temperatures
+  df_sst_summer <- data.frame(sub_1 = df_sst_wide$Summer[match_index[,1]],
+                              sub_2 = df_sst_wide$Summer[match_index[,2]],
+                              sub_3 = df_sst_wide$Summer[match_index[,3]]) %>% 
+    dplyr::mutate(sub = rowMeans(.))
+  
+  # Join in the mean value for the nearest three pixels
+  df_is_res <- df_is_wide %>% 
+    dplyr::mutate(Winter_nearest = df_sst_winter$sub,
+                  Summer_nearest = df_sst_summer$sub,
+                  Winter_dif = Winter - Winter_nearest,
+                  Summer_dif = Summer - Summer_nearest) %>% 
+    dplyr::select(-c(Summer:Summer_nearest)) %>% 
+    gather(key = season, value = dif, 
+           -lon, -lat, -index) %>% 
+    dplyr::mutate(cut = cut(dif, breaks = seq(-8, 1, 1)))
+  
+  # Return the final product
+  return(df_is_res)
+}
 
+SACTN_dif <- product_compare(SACTN_cut, OISST_cut)
+SACTN_dif$season[SACTN_dif$season == "Winter_dif"] <- "Winter"
+SACTN_dif$season[SACTN_dif$season == "Summer_dif"] <- "Summer"
 
 # Plot --------------------------------------------------------------------
 
 # Comparing Winter and SUmmer SACTN and OISST means
-ggplot(OISST_cut, aes(x = lon, y = lat)) +
+dif_1 <- ggplot(OISST_cut, aes(x = lon, y = lat)) +
   geom_raster(aes(fill = temp_cut)) +
+  geom_contour(data = bathy_mask_sub, aes(z = round(depth, -2)), 
+               colour = "grey20", alpha = 0.4) +
   borders(fill = "grey70") +
   geom_point(data = SACTN_cut, size = 3.87, 
              shape = 16, colour = "white") +
@@ -83,4 +133,29 @@ ggplot(OISST_cut, aes(x = lon, y = lat)) +
   labs(x = "", y = "") +
   theme(legend.direction = "horizontal",
         legend.position = "bottom")
+dif_1
+
+# Comparing the difference between coastal pixels
+dif_2 <- ggplot(SACTN_dif, aes(x = lon, y = lat)) +
+  geom_raster(data = OISST_cut, aes(fill = temp_cut)) +
+  geom_contour(data = bathy_mask_sub, aes(z = round(depth, -2)), 
+               colour = "grey20", alpha = 0.4) +
+  borders(fill = "grey70") +
+  geom_point(size = 3.87, shape = 16, colour = "white") +
+  geom_point(size = 2.36, aes(colour = dif), show.legend = F) +
+  facet_wrap(~season) +
+  coord_equal(expand = 0, xlim = c(12, 37), ylim = c(-25, -38)) +
+  # scale_fill_viridis_d() +
+  # scale_colour_viridis_d() +
+  scale_fill_brewer("Temp. (C)", palette = "Spectral", direction = -1, breaks = breaks) +
+  # scale_colour_brewer(palette = "Spectral", direction = -1) +
+  scale_colour_gradient2(low = "blue", high = "red") +
+  # scale_colour_manual(values = pal_1) +
+  # scale_fill_manual("Temp. (C)", values = pal_1) +
+  labs(x = "", y = "") +
+  theme(legend.direction = "horizontal",
+        legend.position = "bottom")
+dif_2
+
+ggarrange(dif_1, dif_2, nrow = 2, ncol = 1, common.legend = TRUE)
 
