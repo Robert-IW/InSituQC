43a19e700d30586bbebd087d5490139484019f7d
Robert William Schlegel <robwschlegel@gmail.com>
2018-06-27 21:51:37 +0200

    A bit more

--- graph/diff.R
+++ graph/diff.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(ggpubr)
 library(FNN)
+library(akima)
 
 
 # Load data ---------------------------------------------------------------
@@ -27,6 +28,29 @@ load("data/bathy_mask.Rdata")
 bathy_mask_sub <- bathy_mask %>% 
   filter(depth <= 0)
 rm(bathy_mask)
+bathy_mask_shelf <- bathy_mask_sub %>% 
+  filter(depth <= -150, depth >= -250)
+bathy_mask_slope <- bathy_mask_sub %>% 
+  filter(depth <= -500, depth >= -1000) %>% 
+  mutate(depth = round(depth, -2))
+bathy_mask_plain <- bathy_mask_sub %>% 
+  filter(depth <= -1000, depth >= -6000) %>% 
+  mutate(depth = round(depth, -3))
+
+## More bathymetry
+bathy <- as.matrix(read.table("data/gebco_sa.asc", sep = "", skip = 6)) # Load in bathymetry plot
+info <- read.table("data/gebco_sa.asc", sep = "", nrows = 6) # Load in plot size parameters
+
+xrng <- seq(from = info[3,2], by = info[5,2], length.out = info[1,2]) # Create x range integers
+yrng <- seq(from = info[4,2], by = info[5,2], length.out = info[2,2]) # Create y range integers
+xy <- expand.grid(xrng, yrng)
+bathy[bathy == info[6,2]] <- NA # Renames error values with NA
+bathy[bathy > 0] <- NA # Removes any values above the surface of the water
+bathy <-  t(bathy[nrow(bathy):1,]) # Adds a column giving row names
+bathy <- as.vector(melt(bathy, value.name = "z", na.rm = FALSE)$z)
+bathy <- as.data.frame(cbind(xy, bathy))
+colnames(bathy) <- c("lon", "lat", "depth"); rm(xy)
+
 
 # Create means ------------------------------------------------------------
 
@@ -109,53 +133,95 @@ SACTN_dif <- product_compare(SACTN_cut, OISST_cut)
 SACTN_dif$season[SACTN_dif$season == "Winter_dif"] <- "Winter"
 SACTN_dif$season[SACTN_dif$season == "Summer_dif"] <- "Summer"
 
+# Join the different dif outputs for plotting
+SACTN_combi <- SACTN_cut %>% 
+  left_join(SACTN_dif, join = c("lon", "lat", "index", "season"))
+
+
+# Interpolate SACTN -------------------------------------------------------
+
+load("data/sa_coast.Rdata")
+# This warning may be ignored
+res_winter <- data.frame(interpp(x = filter(SACTN_combi, season == "Winter")$lon, 
+                                 y = filter(SACTN_combi, season == "Winter")$lat, 
+                                 z = filter(SACTN_combi, season == "Winter")$dif,
+                                 xo = sa_coast$X, yo = sa_coast$Y, linear = TRUE, 
+                                 extrap = FALSE, dupl = "mean"))
+res_winter$index <- 1:nrow(res_winter)
+res_winter$x <- NULL
+res_winter$y <- NULL
+res_winter_approx <- data.frame(approx(x = res_winter$z, method = "linear", 
+                            xout = res_winter$index[is.na(res_winter$z)]))
+colnames(res_winter_approx) <- c("index", "z")
+res_winter_approx <- res_winter_approx %>% 
+  dplyr::select(index, z)
+
+res_winter_full <- rbind(res_winter, res_winter_approx) %>% 
+  na.omit() %>% 
+  arrange(index) %>% 
+  mutate(season = "Winter")
+
+# Now the same for summer
+res_summer <- data.frame(interpp(x = filter(SACTN_combi, season == "Summer")$lon, 
+                                 y = filter(SACTN_combi, season == "Summer")$lat, 
+                                 z = filter(SACTN_combi, season == "Summer")$dif,
+                                 xo = sa_coast$X, yo = sa_coast$Y, linear = TRUE, 
+                                 extrap = FALSE, dupl = "mean"))
+res_summer$index <- 1:nrow(res_summer)
+res_summer$x <- NULL
+res_summer$y <- NULL
+res_summer_approx <- data.frame(approx(x = res_summer$z, method = "linear", 
+                                       xout = res_summer$index[is.na(res_summer$z)]))
+colnames(res_summer_approx) <- c("index", "z")
+res_summer_approx <- res_summer_approx %>% 
+  dplyr::select(index, z)
+
+res_summer_full <- rbind(res_summer, res_summer_approx) %>% 
+  na.omit() %>% 
+  arrange(index) %>% 
+  mutate(season = "Summer")
+
+res_season_full <- rbind(res_winter_full, res_summer_full) %>% 
+  spread(key = season, value = z)
+
 # Plot --------------------------------------------------------------------
 
 # Comparing Winter and SUmmer SACTN and OISST means
 dif_1 <- ggplot(OISST_cut, aes(x = lon, y = lat)) +
   geom_raster(aes(fill = temp_cut)) +
-  geom_contour(data = bathy_mask_sub, aes(z = round(depth, -2)), 
-               colour = "grey20", alpha = 0.4) +
+  # stat_contour(data = bathy, aes(x = lon, y = lat, z = depth, alpha = ..level..), 
+  #              col = "grey70", size = 0.2, binwidth = 500, show.legend = F) +
+  # geom_contour(data = bathy_mask_shelf, aes(z = depth), 
+  #              colour = "grey20", alpha = 0.4, bins = 1) +
   borders(fill = "grey70") +
-  geom_point(data = SACTN_cut, size = 3.87, 
-             shape = 16, colour = "white") +
-  geom_point(data = SACTN_cut, size = 2.36, 
-             aes(colour = temp_cut), show.legend = F) +
-  facet_wrap(~season) +
-  coord_equal(expand = 0, xlim = c(12, 37), ylim = c(-25, -38)) +
-  # scale_fill_viridis_d() +
-  # scale_colour_viridis_d() +
-  scale_fill_brewer("Temp. (C)", palette = "Spectral", direction = -1, breaks = breaks) +
-  scale_colour_brewer(palette = "Spectral", direction = -1) +
-  # scale_colour_gradientn(colours = rainbow(13)) +
-  # scale_colour_manual(values = pal_1) +
-  # scale_fill_manual("Temp. (C)", values = pal_1) +
-  labs(x = "", y = "") +
-  theme(legend.direction = "horizontal",
-        legend.position = "bottom")
-dif_1
-
-# Comparing the difference between coastal pixels
-dif_2 <- ggplot(SACTN_dif, aes(x = lon, y = lat)) +
-  geom_raster(data = OISST_cut, aes(fill = temp_cut)) +
-  geom_contour(data = bathy_mask_sub, aes(z = round(depth, -2)), 
-               colour = "grey20", alpha = 0.4) +
-  borders(fill = "grey70") +
-  geom_point(size = 3.87, shape = 16, colour = "white") +
-  geom_point(size = 2.36, aes(colour = dif), show.legend = F) +
-  facet_wrap(~season) +
+  # geom_point(data = SACTN_dif, size = 3.87, 
+  #            shape = 16, aes(colour = dif)) +
+  # geom_point(data = SACTN_combi, size = 2.36, shape = 21, 
+  #            aes(fill = temp_cut, colour = dif), stroke = 1) +
+  geom_point(data = SACTN_combi, size = 3, shape = 21, 
+             aes(fill = temp_cut), colour = "white", stroke = 0.5) +
+  # geom_point(data = SACTN_combi, aes(colour = dif), 
+  #            shape = "+", size = 4) +
+  facet_wrap(~season, ncol = 2) +
   coord_equal(expand = 0, xlim = c(12, 37), ylim = c(-25, -38)) +
   # scale_fill_viridis_d() +
   # scale_colour_viridis_d() +
   scale_fill_brewer("Temp. (C)", palette = "Spectral", direction = -1, breaks = breaks) +
   # scale_colour_brewer(palette = "Spectral", direction = -1) +
-  scale_colour_gradient2(low = "blue", high = "red") +
-  # scale_colour_manual(values = pal_1) +
-  # scale_fill_manual("Temp. (C)", values = pal_1) +
+  scale_colour_gradient2(low = "black", high = "white") +
   labs(x = "", y = "") +
   theme(legend.direction = "horizontal",
         legend.position = "bottom")
-dif_2
+# dif_1
 
-ggarrange(dif_1, dif_2, nrow = 2, ncol = 1, common.legend = TRUE)
+# Line graph showing difference along coast
+dif_2 <- ggplot(data = res_season_full, aes(x = index)) +
+  geom_ribbon(aes(ymax = Winter, ymin = Summer))
+# dif_2
 
+ggarrange(dif_1, dif_2, ncol = 1, nrow = 2, heights = c(5,1), align = "h", axis = "l")
+gridExtra::grid.arrange(dif_1, dif_2, layout_matrix = cbind(c(1,1,1,NA), c(1,1,1,2),
+                                                            c(1,1,1,2), c(1,1,1,NA)))
+cowplot::plot_grid(dif_1, dif_2, labels = NULL, ncol = 1, 
+                   align = 'v', axis = 'l')
+       
