528bffcec13f6515c7d7ab2dd290a391df37ba7f
Robert-IW <ri.williamson@outlook.com>
2018-11-13 11:52:27 +0200

    Moving to use all time series linear mixed effect adjusted by month observed or interpolated median

--- proc/lm_infillNA.R
+++ proc/lm_infillNA.R
@@ -3,6 +3,21 @@
 # find the longest non-NA segement
 # each matching station is used individually to fill in missing data to
 #   maximize filled in values
+library(tidyverse)
+library(lme4)
+library(forecast)
+
+# normalize data columns using quantiles as min / max (does not work due to apparent seasonal
+#   $ temperature dependency)
+normalit.quan<-function(m){
+  minmax <- quantile(m, c(.1,.9), na.rm=T)
+  min <- minmax[1]
+  max <- minmax[2]
+  (m - min(m, na.rm=T))/(max(m, na.rm=T)-min(m, na.rm=T))
+}
+
+stat.name <- as.character(site_list$index[ind.grp[2]])
+stat.name <- rlang::sym(stat.name)
 
 grp.stat <- as.vector(site_list$index[ind.grp])   # get the station names in the group
 grp.stats <- df.data.stor %>%                     # spread each station as a row
@@ -36,30 +51,122 @@ y.na <- which(is.na(y.in))                                    # record positions
 x.in <- as.ts(grp.stats[names(stat.refs)], frequency=365)
 #names(x.in) <- stat.refs
 
-# for each station in x.in which will be in order
-# set window size for training and forward predicting
-size.window <- 120
-size.interp <- 30
+# Predict using full time series with linear mixed model ------------------
+# Use the full time series to determine monthly coefficients and adjust predicted
+#   values by the difference in medians of the observed - predicted values
+y.med <- grp.stats %>% 
+  dplyr::select(date, stat.name) %>% 
+  mutate(y.in = .[[2]], month = format(date, "%m"), year = format(date, "%Y")) %>% 
+  group_by(month, year) %>% 
+  summarise(month.med = median(y.in, na.rm=T), na.values = sum(is.na(y.in))) %>% 
+  arrange(year, month)
+
+y.med$month.med.update <- mapply(function(x, y) ifelse(x >= 20, NA, y),
+                          y.med$na.values, y.med$month.med)
+
+# interpolate missing medians using seasonal information
+y.med.ts <- ts(y.med$month.med.update, frequency = 12)
+y.med.interp <- na.interp(y.med.ts)
+
+plot(as.numeric(y.med.interp), type = "l", col = "red", lwd = 2,
+     main = paste("Month Median for", stat.name),
+     ylab = "Temperature (°C)", xlab = "Time (months)")
+legend("topleft", legend = c("Original", "Interpolated", "NA > 20"), 
+       inset=c(0,-.1), bty = "n", xpd=TRUE, mar(c(3,3,3,3)),
+       cex = 1, pch=19, col=c("gray30","red","gray70"), horiz = T)
+lines(y.med$month.med, col="black", lwd=2)
+points(y.med$month.med, col = "gray70", pch = 19, cex = 1.5)
+points(as.numeric(y.med.interp), col = "red", pch = 19, cex = 1.5)
+points(y.med$month.med.update, col = "gray30", pch = 19, cex = 1.5)
+abline(v = 1:nrow(y.med), col="gray80")
+abline(v = which(y.med$month=="01"), col = "darkorange")
+abline(v = which(y.med$month=="07"), col = "blue")
 
 for (i in 1:ncol(x.in)){
-  ref.stat <- x.in[,i]
-  ref.name <- dimnames(x.in)[[2]][i]
+  ref.stat <- as.numeric(x.in[,i])
+  ref.stat <- data.frame("ref.stat"=ref.stat, "month"=format(grp.stats$date, "%m"))
+  # normalize using 90th percentile
+  #ref.stat.norm <- normalit.quan(ref.stat)
+  #y.in.norm <- normalit.quan(y.in)
   
-  plot(y.in[!is.na(y.in)], type="l", lwd=2, main=paste(stat.name,"with ref",ref.name),
+  ref.name <- c(dimnames(x.in)[[2]][i],"month")
+  
+  plot(y.in, type="l", lwd=2, main=paste(stat.name,"with ref",ref.name[1]),
        ylab = "Temperature (°C)")
   legend("topleft", legend = c("Original", "Reference", "Naive LM"), 
          inset=c(0,-.1), bty = "n", xpd=TRUE, mar(c(3,3,3,3)),
          cex = 1, lty=1, lwd=3, col=c("black","blue","red"), horiz = T)
-  lines(ref.stat[!is.na(y.in)], col="blue")
+  lines(ref.stat[,1], col="blue")
   
   # 'naive' lm impute
-  df.in.train <- data.frame(cbind(y.in, ref.stat))
-  fit.reg <- lmer(y.in ~ ref.stat + (1|ref.stat), data = df.in.train)
-  df.in.pred <- data.frame(df.in.train[,-1])
-  names(df.in.pred) <- ref.name
-  pred.reg <- predict(fit.reg, newdata = df.in.pred)
-  lines(pred.reg, col = "red")
+  df.in.train <- ref.stat %>% 
+    mutate(y.in=y.in)
+  
+  #names(df.in.train) <- c("y.in", "ref.stat", "month")
+  #fit.reg <- lm(y.in ~ ., data = df.in.train)
+  fit.reg <- lmer(y.in ~ ref.stat + (1|month), data = df.in.train)
+  
+  df.in.pred <- df.in.train %>% 
+    dplyr::select(ref.stat, month)
+  #names(df.in.pred) <- ref.name
+  pred.reg <- predict(fit.reg, newdata = df.in.pred, allow.new.levels = T)
+  
+  # create a monthly median from the observation data andl linearly interpolate missing months
+  #   with too few data
+  cnt = 1
+  pred.adj <- vector(mode = "numeric", length = length(y.in))
+  while ((cnt + 30) < length(y.in)){
+    # if there is not enough data for median in observations use interpolated median
+    
+    diff <- median(y.in[cnt:(cnt+29)], na.rm = T) - median(pred.reg[cnt:(cnt+29)], na.rm = T)
+    pred.adj[cnt:(cnt+29)] <- pred.reg[cnt:(cnt+29)] - diff
+    cnt = cnt + 30
+  }
+ 
+  lines(pred.reg, col = "purple")
+  lines(pred.adj, col = "darkorange")
 
+  
+  
+# Train 60 predict month 1 year  ahead ------------------------------------
+#   this fails with too few records in the 60 day interval
+
+  cnt = 1
+  interp.forward <- vector(mode = "numeric", length = length(y.in))
+  while (cnt + 379 < length(y.in)){
+    y.in.sub <- y.in[cnt:(cnt+59)]
+    x.in.sub <- ref.stat[cnt:(cnt+59),]
+    
+    if (sum(is.na(x.in.sub$ref.stat))/60 > 0.6 | sum(is.na(y.in.sub))/60 > 0.6){
+      print("Too few")
+      cnt <- cnt + 30
+      next
+    }
+    
+    df.in.train <- data.frame(cbind(y.in.sub, x.in.sub$ref.stat))
+    names(df.in.train) <- c("y.in.sub", "ref.stat.sub")
+    df.in.pred <- data.frame(ref.stat$ref.stat[(cnt+350):(cnt+379)])
+    names(df.in.pred) <- "ref.stat.sub"
+    
+    fit.reg <- lm(y.in.sub ~ ref.stat.sub, data = df.in.train)
+    pred.reg <- predict.lm(fit.reg, newdata = df.in.pred)
+    interp.forward[(cnt+350):(cnt+379)] <- pred.reg
+    
+    cnt <- cnt + 30
+  }
+  
+
+# Similar to above --------------------------------------------------------
+# Predicts moving forward and the predicts moving backwards and looks for
+# Largest errors between the two predicted graphs
+# Again runs into problems with the number of observations for training
+  
+  # for each station in x.in which will be in order
+  # set window size for training and forward predicting
+  size.window <- 60     
+  size.interp <- 30
+  size.gap <- 350  
+  
   # function to interpolate using sliding LM forward/backward, using 4 month window
   #   to predict 30 day ahead
   interp.forback <- function(y.in, ref.stat){
@@ -73,21 +180,23 @@ for (i in 1:ncol(x.in)){
     while (cnt.en < length(y.in)){
       if ((cnt.en + size.interp) < (length(y.in)-(2*size.interp))){
         y.in.sub <- y.in[cnt.st:cnt.en]
-        x.in.sub <- ref.stat[cnt.st:cnt.en]
+        x.in.sub <- ref.stat[cnt.st:cnt.en,]
         df.in.train <- data.frame(cbind(y.in.sub, x.in.sub))
-        df.in.pred <- data.frame(ref.stat[cnt.en:(cnt.en + size.interp -1)])
+        df.in.pred <- data.frame(ref.stat[cnt.en:(cnt.en + size.interp -1),])
         names(df.in.pred) <- names(df.in.train)[-1]
         
         # if there is insufficient data use previous lm
         if (sum(is.na(df.in.train$y.in.sub)) / nrow(df.in.train) < 0.66){
-          fit.reg <- lm(y.in.sub ~ ., data = df.in.train)
+          fit.reg <- lmer(y.in.sub ~ ref.stat + (1|month), data = df.in.train)
         } else {
           print("Reverting to previous LM")
         }
         
         # if the first interation, use model prediction on first 120 days
         if (cnt.st == 1){
-          pred.first <- predict.lm(fit.reg, newdata=data.frame(df.in.train[,-1]))
+          pred.first <- predict(fit.reg,
+                                   newdata=data.frame(df.in.train[,-1]),
+                                   allow.new.levels = T)
           interp.forward[cnt.st:cnt.en] <- pred.first
           rm(pred.first)
           cnt.st <- cnt.st + size.interp
@@ -95,7 +204,8 @@ for (i in 1:ncol(x.in)){
           print("First interation complete")
           next
         }
-        pred.reg <- predict.lm(fit.reg, newdata = df.in.pred)
+        pred.reg <- predict(fit.reg, newdata = df.in.pred,
+                            allow.new.levels = T)
         interp.forward[(cnt.en+1):(cnt.en + size.interp)] <- pred.reg
         
         cnt.st <- cnt.st + size.interp
@@ -108,13 +218,14 @@ for (i in 1:ncol(x.in)){
           size.interp.tmp <- length(y.in) - cnt.en
           
           y.in.sub <- y.in[cnt.st:cnt.en]
-          x.in.sub <- ref.stat[cnt.st:cnt.en]
+          x.in.sub <- ref.stat[cnt.st:cnt.en,]
           df.in.train <- data.frame(cbind(y.in.sub, x.in.sub))
-          df.in.pred <- data.frame(ref.stat[(cnt.en+1):(cnt.en + size.interp.tmp -1)])
-          names(df.in.pred) <- names(df.in.train)[-1]
+          df.in.pred <- data.frame(ref.stat[(cnt.en+1):(cnt.en + size.interp.tmp -1),])
+          #names(df.in.pred) <- names(df.in.train)[-1]
           
-          fit.reg <- lm(y.in.sub ~ ., data = df.in.train)
-          pred.reg <- predict.lm(fit.reg, newdata = df.in.pred)
+          fit.reg <- lmer(y.in.sub ~ ref.stat + (1|month), data = df.in.train)
+          pred.reg <- predict(fit.reg, newdata = df.in.pred,
+                                 allow.new.levels = T)
           interp.forward[(cnt.en+1):(cnt.en + size.interp.tmp -1)] <- pred.reg
           print("Reached the end of the series")
           break
@@ -124,16 +235,40 @@ for (i in 1:ncol(x.in)){
           y.in.sub <- y.in[cnt.st:length(y.in)]
           x.in.sub <- ref.stat[cnt.st:length(y.in)]
           df.in.train <- data.frame(cbind(y.in.sub, x.in.sub))
-          pred.last <- predict.lm(fit.reg, newdata=data.frame(df.in.train[,-1]))
+          pred.last <- predict(fit.reg, newdata=data.frame(df.in.train[,-1]),
+                               allow.new.levels = T)
           interp.forward[cnt.en:length(y.in)] <- pred.last
           rm(pred.last)
           print("Reached the end of the series")
           break
         }
       }
+      # reverse and run backward
+      interp.forward.org <- interp.forward
+      y.in.org <- y.in
+      y.in <- rev(y.in.org)
+      ref.stat.org <- ref.stat
+      ref.stat <- rev(ref.stat)
+      
+      # reset and plot
+      interp.backward <- rev(interp.forward)
+      y.in <- y.in.org
+      ref.stat <- ref.stat.org
+      interp.forward <- interp.forward.org
+      
+      plot(y.in, type="l")
+      lines(interp.forward, col="darkorange")
+      lines(interp.backward, col="chartreuse4")
     }
   }
 }
+
+plot(y.in, type="l")
+lines(interp.forward, col="darkorange")
+lines(interp.backward, col="chartreuse4")
+
+
+# Unknown -----------------------------------------------------------------
 
 
 # # find which time series are best for filling in missing data
